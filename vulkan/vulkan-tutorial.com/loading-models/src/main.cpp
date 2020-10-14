9b66230538ab7718f18382e7ae4e7f03cc2bb830
Alex Todoran <alex-tdrn@protonmail.com>
2020-10-14 14:17:01 +0200

    started loading models chapter

---
+++ vulkan/vulkan-tutorial.com/loading-models/src/main.cpp
@@ -0,0 +1,19 @@
+#include "LoadingModels.h"
+
+#include <iostream>
+
+int main()
+{
+	LoadingModels app;
+
+	try
+	{
+		app.run();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
