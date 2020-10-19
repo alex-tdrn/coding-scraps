103dbafc8197eb8b7aaeadb14796f46e3acd5d14
Alex Todoran <alex-tdrn@protonmail.com>
2020-10-19 17:25:52 +0200

    generating mipmaps chapter started

---
+++ vulkan/vulkan-tutorial.com/07-generating-mipmaps/src/main.cpp
@@ -0,0 +1,19 @@
+#include "GeneratingMipmaps.h"
+
+#include <iostream>
+
+int main()
+{
+	GeneratingMipmaps app;
+
+	try
+	{
+		app.run();
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
